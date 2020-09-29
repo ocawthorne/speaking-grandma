44a8dae9070762aed486f3986fdfbdc1bbb0ec0d
ocawthorne <ocawthorne@gmail.com>
2020-09-29 18:30:43 +0100

    speaking-grandma-092920

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,3 @@
-# Write a speak_to_grandma method.
-
 def speak_to_grandma(input)
     grandma = input == "I LOVE YOU GRANDMA!" ? "I LOVE YOU TOO PUMPKIN!" : (input == input.upcase ? "NO, NOT SINCE 1938!" : "HUH?! SPEAK UP, SONNY!")
 end
